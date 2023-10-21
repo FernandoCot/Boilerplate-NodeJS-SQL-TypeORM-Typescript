14189e4c2ddb50eb7ed63672cf303cb5174abc6d
Fernando Calixto <fernandocalixtodev@gmail.com>
2023-10-20 23:10:17 -0300

    feat: creating a new migration to generate the "User" table

---
+++ src/migration/1697852109494-CreateUserTable.ts
@@ -0,0 +1,50 @@
+// Core
+import {
+  Table,
+  QueryRunner,
+  MigrationInterface,
+} from "typeorm";
+
+export class CreateUserTable1697852109494 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "user",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user");
+  }
+}
